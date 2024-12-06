c09339daa88536666fc6b77bb4de58b8904ab1b8
LucasT040 <82636735+LucasT040@users.noreply.github.com>
2024-12-06 11:08:11 -0300

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,85 @@
 namespace SpriteKind {
     export const portal = SpriteKind.create()
 }
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    mySprite,
+    [img`
+        . . . . . . f f f f . . . . . . 
+        . . . . f f e e e e f f . . . . 
+        . . . f e e e f f e e e f . . . 
+        . . f f f f f 2 2 f f f f f . . 
+        . . f f e 2 e 2 2 e 2 e f f . . 
+        . . f e 2 f 2 f f 2 f 2 e f . . 
+        . . f f f 2 2 e e 2 2 f f f . . 
+        . f f e f 2 f e e f 2 f e f f . 
+        . f e e f f e e e e f e e e f . 
+        . . f e e e e e e e e e e f . . 
+        . . . f e e e e e e e e f . . . 
+        . . e 4 f f f f f f f f 4 e . . 
+        . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+        . . 4 4 f 4 4 4 4 4 4 f 4 4 . . 
+        . . . . . f f f f f f . . . . . 
+        . . . . . f f . . f f . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . f f f f . . . . . . . 
+        . . . f f e e e e f f . . . . . 
+        . . f e e e f f e e e f . . . . 
+        . . f f f f 2 2 f f f f . . . . 
+        . f f e 2 e 2 2 e 2 e f f . . . 
+        . f e 2 f 2 f f f 2 f e f . . . 
+        . f f f 2 f e e 2 2 f f f . . . 
+        . f e 2 f f e e 2 f e e f . . . 
+        f f e f f e e e f e e e f f . . 
+        f f e e e e e e e e e e f d f . 
+        . . f e e e e e e e e f f b f . 
+        . . e f f f f f f f f 4 f b f . 
+        . . 4 f 2 2 2 2 2 e d d f c f . 
+        . . e f f f f f f e e 4 f f . . 
+        . . . f f f . . . . . . . . . . 
+        `,img`
+        . . . . . f f f f . . . . . . . 
+        . . . f f e e e e f f . . . . . 
+        . . f e e e f f e e e f . . . . 
+        . f f f f f 2 2 f f f f f . . . 
+        . f f e 2 e 2 2 e 2 e f f . . . 
+        . f e 2 f 2 f f 2 f 2 e f . . . 
+        . f f f 2 2 e e 2 2 f f f . . . 
+        f f e f 2 f e e f 2 f e f f . . 
+        f e e f f e e e e f e e e f . . 
+        . f e e e e e e e e e e f . . . 
+        . . f e e e e e e e e f . . . . 
+        . e 4 f f f f f f f f 4 e . . . 
+        . 4 d f 2 2 2 2 2 2 f d 4 . . . 
+        . 4 4 f 4 4 4 4 4 4 f 4 4 . . . 
+        . . . . f f f f f f . . . . . . 
+        . . . . f f . . f f . . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . f f f f . . . . . . . 
+        . . . f f e e e e f f . . . . . 
+        . . f e e e f f e e e f . . . . 
+        . . f f f f 2 2 f f f f . . . . 
+        . f f e 2 e 2 2 e 2 e f f . . . 
+        . f e f 2 f f f 2 f 2 e f . . . 
+        . f f f 2 2 e e f 2 f f f . . . 
+        . f e e f 2 e e f f 2 e f . . . 
+        . f e e e f e e e f f e f f . . 
+        . f e e e e e e e e e e f f . . 
+        . f f e e e e e e e e f . . . . 
+        . f 4 f f f f f f f f e . . . . 
+        . f d d e 2 2 2 2 2 f 4 . . . . 
+        . f 4 e e f f f f f f e . . . . 
+        . . . . . . . . f f f . . . . . 
+        `],
+    100,
+    true
+    )
+})
+controller.down.onEvent(ControllerButtonEvent.Released, function () {
+    animation.stopAnimation(animation.AnimationTypes.All, mySprite)
+})
 function createPlayerTwo () {
     mySprite2 = sprites.create(img`
         . . . . . . . c c c . . . . . . 
@@ -181,6 +260,85 @@ function createEnemies () {
     true
     )
 }
+controller.up.onEvent(ControllerButtonEvent.Released, function () {
+    animation.stopAnimation(animation.AnimationTypes.All, mySprite)
+})
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    mySprite,
+    [img`
+        . . . . . . f f f f . . . . . . 
+        . . . . f f f 2 2 f f f . . . . 
+        . . . f f f 2 2 2 2 f f f . . . 
+        . . f f f e e e e e e f f f . . 
+        . . f f e 2 2 2 2 2 2 e e f . . 
+        . . f e 2 f f f f f f 2 e f . . 
+        . . f f f f e e e e f f f f . . 
+        . f f e f b f 4 4 f b f e f f . 
+        . f e e 4 1 f d d f 1 4 e e f . 
+        . . f f f f d d d d d e e f . . 
+        . f d d d d f 4 4 4 e e f . . . 
+        . f b b b b f 2 2 2 2 f 4 e . . 
+        . f b b b b f 2 2 2 2 f d 4 . . 
+        . . f c c f 4 5 5 4 4 f 4 4 . . 
+        . . . f f f f f f f f . . . . . 
+        . . . . . f f . . f f . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . f f f f . . . . . . 
+        . . . . f f f 2 2 f f f . . . . 
+        . . . f f f 2 2 2 2 f f f . . . 
+        . . f f f e e e e e e f f f . . 
+        . . f f e 2 2 2 2 2 2 e e f . . 
+        . f f e 2 f f f f f f 2 e f f . 
+        . f f f f f e e e e f f f f f . 
+        . . f e f b f 4 4 f b f e f . . 
+        . f f e 4 1 f d d f 1 4 e f . . 
+        f d f f e 4 d d d d 4 e f e . . 
+        f b f e f 2 2 2 2 e d d 4 e . . 
+        f b f 4 f 2 2 2 2 e d d e . . . 
+        f c f . f 4 4 5 5 f e e . . . . 
+        . f f . f f f f f f f . . . . . 
+        . . . . f f f . . . . . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . f f f f . . . . . . 
+        . . . . f f f 2 2 f f f . . . . 
+        . . . f f f 2 2 2 2 f f f . . . 
+        . . f f f e e e e e e f f f . . 
+        . . f e e 2 2 2 2 2 2 e f f . . 
+        . f f e 2 f f f f f f 2 e f f . 
+        . f f f f f e e e e f f f f f . 
+        . . f e f b f 4 4 f b f e f . . 
+        . . f e 4 1 f d d f 1 4 e f . . 
+        . . e f f f f d d d 4 e f . . . 
+        . . f d d d d f 2 2 2 f e f . . 
+        . . f b b b b f 2 2 2 f 4 e . . 
+        . . f b b b b f 5 4 4 f . . . . 
+        . . . f c c f f f f f f . . . . 
+        . . . . f f . . . f f f . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . f f f f . . . . . . 
+        . . . . f f f 2 2 f f f . . . . 
+        . . . f f f 2 2 2 2 f f f . . . 
+        . . f f f e e e e e e f f f . . 
+        . . f e e 2 2 2 2 2 2 e f f . . 
+        . f f e 2 f f f f f f 2 e f f . 
+        . f f f f f e e e e f f f f f . 
+        . . f e f b f 4 4 f b f e f . . 
+        . . f e 4 1 f d d f 1 4 e f f . 
+        . . e f e 4 d d d d 4 e f f d f 
+        . . e 4 d d e 2 2 2 2 f e f b f 
+        . . . e d d e 2 2 2 2 f 4 f b f 
+        . . . . e e f 5 5 4 4 f . f c f 
+        . . . . . f f f f f f f . f f . 
+        . . . . . . . . . f f f . . . . 
+        `],
+    100,
+    true
+    )
+})
 function createPlayerOne () {
     mySprite = sprites.create(img`
         . . . . . . . . . . . . . . . . 
@@ -202,80 +360,6 @@ function createPlayerOne () {
         `, SpriteKind.Player)
     mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.One), mySprite)
     mp.moveWithButtons(mp.playerSelector(mp.PlayerNumber.One))
-    animation.runImageAnimation(
-    mySprite,
-    [img`
-        . . . . . . f f f f . . . . . . 
-        . . . . f f e e e e f f . . . . 
-        . . . f e e e f f e e e f . . . 
-        . . f f f f f 2 2 f f f f f . . 
-        . . f f e 2 e 2 2 e 2 e f f . . 
-        . . f e 2 f 2 f f 2 f 2 e f . . 
-        . . f f f 2 2 e e 2 2 f f f . . 
-        . f f e f 2 f e e f 2 f e f f . 
-        . f e e f f e e e e f e e e f . 
-        . . f e e e e e e e e e e f . . 
-        . . . f e e e e e e e e f . . . 
-        . . e 4 f f f f f f f f 4 e . . 
-        . . 4 d f 2 2 2 2 2 2 f d 4 . . 
-        . . 4 4 f 4 4 4 4 4 4 f 4 4 . . 
-        . . . . . f f f f f f . . . . . 
-        . . . . . f f . . f f . . . . . 
-        `,img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . f f f f . . . . . . . 
-        . . . f f e e e e f f . . . . . 
-        . . f e e e f f e e e f . . . . 
-        . . f f f f 2 2 f f f f . . . . 
-        . f f e 2 e 2 2 e 2 e f f . . . 
-        . f e 2 f 2 f f f 2 f e f . . . 
-        . f f f 2 f e e 2 2 f f f . . . 
-        . f e 2 f f e e 2 f e e f . . . 
-        f f e f f e e e f e e e f f . . 
-        f f e e e e e e e e e e f d f . 
-        . . f e e e e e e e e f f b f . 
-        . . e f f f f f f f f 4 f b f . 
-        . . 4 f 2 2 2 2 2 e d d f c f . 
-        . . e f f f f f f e e 4 f f . . 
-        . . . f f f . . . . . . . . . . 
-        `,img`
-        . . . . . f f f f . . . . . . . 
-        . . . f f e e e e f f . . . . . 
-        . . f e e e f f e e e f . . . . 
-        . f f f f f 2 2 f f f f f . . . 
-        . f f e 2 e 2 2 e 2 e f f . . . 
-        . f e 2 f 2 f f 2 f 2 e f . . . 
-        . f f f 2 2 e e 2 2 f f f . . . 
-        f f e f 2 f e e f 2 f e f f . . 
-        f e e f f e e e e f e e e f . . 
-        . f e e e e e e e e e e f . . . 
-        . . f e e e e e e e e f . . . . 
-        . e 4 f f f f f f f f 4 e . . . 
-        . 4 d f 2 2 2 2 2 2 f d 4 . . . 
-        . 4 4 f 4 4 4 4 4 4 f 4 4 . . . 
-        . . . . f f f f f f . . . . . . 
-        . . . . f f . . f f . . . . . . 
-        `,img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . f f f f . . . . . . . 
-        . . . f f e e e e f f . . . . . 
-        . . f e e e f f e e e f . . . . 
-        . . f f f f 2 2 f f f f . . . . 
-        . f f e 2 e 2 2 e 2 e f f . . . 
-        . f e f 2 f f f 2 f 2 e f . . . 
-        . f f f 2 2 e e f 2 f f f . . . 
-        . f e e f 2 e e f f 2 e f . . . 
-        . f e e e f e e e f f e f f . . 
-        . f e e e e e e e e e e f f . . 
-        . f f e e e e e e e e f . . . . 
-        . f 4 f f f f f f f f e . . . . 
-        . f d d e 2 2 2 2 2 f 4 . . . . 
-        . f 4 e e f f f f f f e . . . . 
-        . . . . . . . . f f f . . . . . 
-        `],
-    100,
-    true
-    )
 }
 function TestLvL1 () {
     createPlayerOne()
@@ -285,6 +369,6 @@ function TestLvL1 () {
 }
 let mySprite4: Sprite = null
 let mySprite3: Sprite = null
-let mySprite: Sprite = null
 let mySprite2: Sprite = null
+let mySprite: Sprite = null
 TestLvL1()
